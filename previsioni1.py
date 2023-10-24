776ce3d0f07aebe19c24ca9f85b993cf633d3bcc
Angelo Frizzo <75337724+angelofrizzo@users.noreply.github.com>
2023-10-24 09:50:44 +0200

    aaaa

--- previsioni1.py
+++ previsioni1.py
@@ -1,13 +1,7 @@
 import numpy as np
 
 resistenze = np.array([215.07, 117.97, 324.59, 575.12, 465.16])
+tau10 = np.array([232e-7, 424e-7, 154e-7, 87e-7, 107e-7])
 
-R1 = 215.07
-R2 = 117.97
-R3 = 324.59
-R4 = 575.12
-R5 = 465.16
-
-L = 7.5e-3
-
-tau = L / resistenze
+for i in tau10:
+    print("Tau10 di {tau}".format(tau = i*10))
