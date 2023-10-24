17c4c674ba817294db9010b5a6022a1fa9daea9e
Angelo Frizzo <75337724+angelofrizzo@users.noreply.github.com>
2023-10-24 09:34:51 +0200

    aaa

---
+++ previsioni1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+resistenze = np.array([215.07, 117.97, 324.59, 575.12, 465.16])
+
+R1 = 215.07
+R2 = 117.97
+R3 = 324.59
+R4 = 575.12
+R5 = 465.16
+
+L = 7.5e-3
+
+tau = L / resistenze
